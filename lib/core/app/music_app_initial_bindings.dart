94559b33d9c8b89bf46568e681b09ee4ec1bb936
stobertonf <stobertonf@gmail.com>
2025-03-16 17:37:14 -0300

    feat(Audio-Service): "Creating The Music Player Controller File."

--- lib/core/app/music_app_initial_bindings.dart
+++ lib/core/app/music_app_initial_bindings.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:music_app/core/services/api_service.dart';
 import 'package:music_app/core/services/audio_player_service_impl.dart';
+import 'package:music_app/shared/features/music_app/presentation/controllers/music_player_controller.dart';
 
 class MusicAppInitialBindings extends Bindings {
   @override
