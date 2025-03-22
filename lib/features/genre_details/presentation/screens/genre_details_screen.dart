cc144ade18d839b13c7d50f5d791363fe6b40639
stobertonf <stobertonf@gmail.com>
2025-03-22 19:30:01 -0300

    feat(Genre-List-Screen): "Creating The Genre Detaisl Screen File."

---
+++ lib/features/genre_details/presentation/screens/genre_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GenreDetaislScreen extends StatelessWidget {
+  static const routeName = "/genre-details";
+
+  const GenreDetaislScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
